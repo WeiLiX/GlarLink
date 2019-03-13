b1af7eff515ab8243ba4743a11d8aa11b9d1ba76
liwei <wei.li@dnurse.cn>
2019-03-13 10:11:31 +0800

    first commit

---
+++ GlarLinkTest/CBPeripheral+MacAddr.h
@@ -0,0 +1,17 @@
+//
+//  CBPeripheral+MacAddr.h
+//  GlarLinkTest
+//
+//  Created by Wei Li on 2018/12/13.
+//  Copyright © 2018 Wei Li. All rights reserved.
+//
+
+#import <CoreBluetooth/CoreBluetooth.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CBPeripheral (MacAddr)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GlarLinkTest/CBPeripheral+MacAddr.m
@@ -0,0 +1,13 @@
+//
+//  CBPeripheral+MacAddr.m
+//  GlarLinkTest
+//
+//  Created by Wei Li on 2018/12/13.
+//  Copyright © 2018 Wei Li. All rights reserved.
+//
+
+#import "CBPeripheral+MacAddr.h"
+
+@implementation CBPeripheral (MacAddr)
+
+@end
